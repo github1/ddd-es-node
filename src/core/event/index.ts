40ce442c44d7a4a19bc8c5c015e39017518f3b0d
github1 <john.collier4@gmail.com>
2020-11-07 22:12:00 -0500

    fix tests

--- src/core/event/index.ts
+++ src/core/event/index.ts
@@ -1,7 +1,5 @@
 export * from './entity-event';
 export * from './event-bus';
 export * from './event-dispatcher';
-export {eventDispatcherWithModifiers} from './event-dispatcher';
 export * from './event-store';
-export {EventStoreSeeded} from './event-store';
 export {MemoryEventStore} from './event-store-memory';
